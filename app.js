14a15a7bf26d45ff3a257a6e935eafdb4a94a8cc
Jonathan-Caillon <j.caillon@codeur.online>
2021-05-28 15:50:25 +0200

    feat:confettis

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const play = () => {
 
     // nombre aléatoire
     const randomNumber = Math.floor(Math.random() * 101);
-    const totalVies = 5;
+    const totalVies = 6;
     let vies = totalVies;
     console.log(randomNumber)
 
@@ -51,7 +51,7 @@ const play = () => {
             essayerBtn.style.display = "none";
             input.style.display = "none";
             label.style.display = "none";
-        
+            fiesta()
         }
 
         if(valeurInput !== randomNumber) {
@@ -120,4 +120,43 @@ const play = () => {
 
 }
 play();
+
+
+
+// Confettis
+
+const containerSlot = document.querySelector(".slot");
+
+const emojis = ["✨","🎆","🎇","🎈"];
+
+function fiesta() {
+
+    for(let i = 0; i < 50; i++){
+        const confetti = document.createElement("div");
+        confetti.innerText = emojis[Math.floor(Math.random()* emojis.length)];
+        containerSlot.appendChild(confetti)
+    }
+
+    animateConfettis();
+    
+}
+
+function animateConfettis() {
+
+    const TLCONF = gsap.timeline()
+
+    TLCONF
+    .to(".slot div", {
+        y: "random(-100,100)",
+        x: "random(-100,100)",
+        z: "random(0,1000)",
+        rotation: "random(-90,90)",
+        duration: 1
+    })
+    .to(".slot div", {autoAlpha: 0, duration: 0.3},
+    "-=0.2")
+    .add(() => {
+        containerSlot.innerHTML = "";
+    })
+}
 
